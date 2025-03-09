a5a2f63f2d5122e62b58956bd6d54949e1a5f013
Eyyub Saral <eyyubsrl04@gmail.com>
2025-03-09 12:40:33 +0300

    BasitYemek

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles.css";
-import Header from "./header";
+import Header from "./Header";
 import Footer from "./Footer";
-import Main from "./main";
+import Main from "./Main";
 
 /* Challenge: 
 
